95a888a3aea51338bea99891d3dead7e717e9050
unknown <baldissaramatheus@gmail.com>
2021-03-07 13:45:30 -0300

    Atualiza casos de teste

--- test/ProcessadorBoletosTest.java
+++ test/ProcessadorBoletosTest.java
@@ -1,16 +1,30 @@
 import org.junit.jupiter.api.Assertions;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ProcessadorBoletosTest {
 
 	@Test
-	void TestaSeBoletosResultamCorretamenteEmFaturaPaga() {
+	@DisplayName("Testa se boletos cuja soma de valores pagos é igual ao valor da fatura resulta em fatura paga")
+	void TestaFaturaPagaParaValoresIguais() {
+
+	}
+	
+	@Test
+	@DisplayName("Testa se boletos cuja soma de valores pagos é maior que valor da fatura resulta em fatura paga")
+	void TestaFaturaPagaParaValoresMaiores() {
 		Assertions.fail();
 	}
 	
 	@Test
-	void TestaSeBoletosResultamCorretamenteEmFaturaNaoPaga() {
+	@DisplayName("Testa se boletos cuja soma de valores pagos é menor que o valor da fatura resulta em fatura paga")
+	void TestaFaturaPagaParaValoresMenores() {
+		Assertions.fail();
+	}
+	
+	@Test
+	@DisplayName("Testa se fatura possui o tipo correto de pagamento")
+	void TestaTipoDePagamento() {
 		Assertions.fail();
 	}
 
